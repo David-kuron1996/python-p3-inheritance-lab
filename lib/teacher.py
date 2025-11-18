655ff5f6bd5505b66aecae8e62b15c3550537ba0
David <davidkuron1996@gmail.com>
2025-11-18 21:42:57 +0300

    add this file

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,10 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]
+    
     def teach(self):
+        return self.knowledge[0]  # Return the first item in knowledge list
         pass
